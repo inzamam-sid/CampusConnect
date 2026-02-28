5241887af0f569136c39675c433e9ea31d189c6b
inzamam-siddiqui <siddiquiinzmam319@gmail.com>
2026-02-28 23:38:51 +0530

    Refactor postRoutes to include like, add comment, and delete comment routes

--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -4,6 +4,8 @@ const {
   createPost,
   getFeed
 } = require("../controllers/postController");
+const { toggleLike } = require("../controllers/postController");
+const { addComment, deleteComment } = require("../controllers/postController");
 
 const multer = require("multer");
 
@@ -22,5 +24,19 @@ router.post("/", authMiddleware, upload.single("image"), createPost);
 
 // Get all posts (Feed)
 router.get("/feed", authMiddleware, getFeed);
+
+// Like or Unlike post
+router.put("/like/:id", authMiddleware, toggleLike);
+
+// Add comment
+router.post("/comment/:id", authMiddleware, addComment);
+
+// Delete comment
+router.delete("/comment/:postId/:commentId", authMiddleware, deleteComment);
+
+// router.get("/debug", (req, res) => {
+//   res.json({ message: "Post routes working" });
+// });
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5OGI0OTNhN2QyNWYxNjE0MDc5YWQyNCIsImlhdCI6MTc3MjMwMTkwN30.xgiGgiVwA_mUp_lDWPHWJp0dslPd6eRmXg_pTLAP4_w
 
 module.exports = router;
